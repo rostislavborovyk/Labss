4b20cf25b94ad711d972f1396faa1dcd13a47a6d
Ростислав <rostmoguchiy@gmail.com>
2019-03-21 19:51:33 +0200

    lab_2

---
+++ src/lab_4/Furniture.java
@@ -0,0 +1,56 @@
+package lab_4;
+// C11 = 8 - Визначити клас меблі, який складається як мінімум з 5-и полів.
+public class Furniture {
+    private String type;
+    private int cost;
+    private String material;
+    private String origin;
+    private int mass;
+    Furniture(String type, int cost, String material, String origin, int mass){
+        this.type = type;
+        this.cost = cost;
+        this.material = material;
+        this.origin = origin;
+        this.mass = mass;
+    }
+    @Override
+    public String toString(){
+        return type + "\t\t" + cost + " uah" + "\t\t" +    //type or this.type ?
+                material + "\t\t" + origin + "\t\t" + mass + " kg";
+    }
+
+    String getType(){
+        return this.type;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+
+    int getCost(){
+        return this.cost;
+    }
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+
+    String getMaterial(){
+        return this.material;
+    }
+    public void setMaterial(String material){
+        this.material = material;
+    }
+
+    int getMass(){
+        return this.mass;
+    }
+    public void setMass(int mass){
+        this.mass = mass;
+    }
+
+    String getOrigin(){
+        return this.origin;
+    }
+     void setOrigin(String origin){
+        this.origin = origin;
+    }
+}
---
+++ src/lab_4/main_program.java
@@ -0,0 +1,35 @@
+package lab_4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class main_program {
+
+    public static void main(String[] args) {
+//        Furniture example = new Furniture("Chair", 1000,
+//                                        "Wood", "France", 10);
+        Furniture[] furnitureArray = new Furniture[3];
+        furnitureArray[0] = new Furniture("Chair", 1000,
+                "Wood", "France", 10);
+        furnitureArray[1] = new Furniture("Sofa", 5000,
+                "Wood", "Spain", 80);
+        furnitureArray[2] = new Furniture("Table", 2000,
+                "Plastic", "Ukraine", 15);
+        System.out.println("Array before sorting");
+        for (Furniture elem : furnitureArray) {
+            System.out.println(elem);
+        }
+        System.out.println("\nArray sorted with cost");
+        Arrays.sort(furnitureArray, Comparator.comparing(Furniture :: getCost));
+        for (Furniture elem : furnitureArray) {
+            System.out.println(elem);
+        }
+        System.out.println("\nArray sorted with origin");
+        Arrays.sort(furnitureArray, Comparator.comparing(Furniture :: getOrigin));
+        for (Furniture elem : furnitureArray) {
+            System.out.println(elem);
+        }
+
+    }
+
+}
